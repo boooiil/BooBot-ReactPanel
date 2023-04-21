a56cfff8c3fb2aae0254b860e4bb3341990baf4b
boooiil <boooiil@outlook.com>
2023-04-21 05:09:08 -0400

    react defaults

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import NotifyTabular from 'components/guild/NotifyTabular';
+import './App.css';
+import ListGuildsTabular from 'components/guild/ListGuldsTabular';
+
+function App() {
+  return (
+    <ListGuildsTabular />
+  );
+}
+
+export default App;
