5245c7208cd5783a375ef72b7633722387436599
boooiil <boooiil@outlook.com>
2023-05-04 10:59:06 -0400

    finalize

--- src/components/guild/ListGuldsTabular.tsx
+++ src/components/guild/ListGuldsTabular.tsx
@@ -1,7 +1,11 @@
 import { Component } from "react";
 import { Guild } from "lib/BooBot/classes/Guild";
+import Head from "components/generic/Head";
+import IndividualizedGuildView from "./IndividualizedGuildView";
 
 export default class ListGuildsTabular extends Component<{}, { guilds: Guild[] }> {
+
+    guildIndex: number = -1;
 
     constructor(props: any) {
         super(props);
@@ -14,47 +18,79 @@ export default class ListGuildsTabular extends Component<{}, { guilds: Guild[] }
             .then(data => { return this.setState({ guilds: data }) });
     };
 
-    redirect(url: string, id: string) {
-        (window.location.href = url + id);
-        return undefined;
+    validateURLPath(): boolean {
+
+        let path = window.location.pathname.split('/');
+        if (path.length === 3 && path[1] === 'guilds') {
+            let id = path[2];
+            let index = this.state.guilds.findIndex((guild: Guild) => {
+                return guild.id === id;
+            });
+            if (index !== -1) {
+                this.guildIndex = index;
+                return true;
+            }
+        }
+
+        else {
+
+            window.history.replaceState(null, "", "/");
+
+        }
+
+        return false;
+
+    }
+
+    redirect(index: number) {
+        //TODO: RENAME THIS TO SOMETHING MORE APPROPRIATE
+        this.guildIndex = index;
+        window.history.replaceState(null, "", "/guilds/" + this.state.guilds[index].id);
+        this.forceUpdate();
     }
 
     printGuilds(): JSX.Element[] {
-        return this.state.guilds.map((guild: Guild) => {
+        return this.state.guilds.map((guild: Guild, index: number) => {
 
             return (
                 <tr key={guild.id}>
                     <td>{guild.name}</td>
                     <td>{guild.id}</td>
-                    <td><button onClick={() => this.redirect('/view/', guild.id)}>View</button></td>
-                    <td><button onClick={() => this.redirect('/modify/', guild.id)}>Modify</button></td>
-                    <td><button onClick={() => this.redirect('/delete/', guild.id)}>Delete</button></td>
+                    <td><button onClick={() => this.redirect(index)}>View</button></td>
+                    <td><button onClick={() => this.redirect(index)}>Modify</button></td>
+                    <td><button onClick={() => this.redirect(index)}>Delete</button></td>
                 </tr>
             )
 
         });
     }
 
-
     render() {
 
-        return (
+        if (this.guildIndex !== -1 || this.validateURLPath()) {
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>Guild Name</th>
-                        <th>Guild ID</th>
-                        <th>View</th>
-                        <th>Modify</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.printGuilds()}
-                </tbody>
-            </table>
+            return (
+                <div>
+                    <Head title="Guild" />
+                    <IndividualizedGuildView guild={this.state.guilds[this.guildIndex]} />
+                </div>
 
-        );
+            );
+        }
+
+        else {
+            return (
+                <div>
+                    <Head title="Guilds" />
+                    <table>
+                        <tbody>
+                            {this.printGuilds()}
+                        </tbody>
+                    </table>
+                </div>
+            );
+
+        }
+
     }
 }
